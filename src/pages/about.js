ca3bff7222f472552c5278726ae742f8ffbceb06
Anastasia Shpit <imshpit@gmail.com>
2020-11-20 20:30:26 +0200

    added blog, contact and about pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Hi there! You are on my lifestyle & tech blog!</p>
+        </div>
+    )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>Here are some ways to reach me</p>
+            <ul>
+                <li>Email: nastyawebdev@gmail.com</li>
+                <li>GitHub: @nastyacodes</li>
+                <li>Instagram: @nastyacodes</li>
+                <li>Twitter: @nastyacodes</li>
+            </ul>
+        </div>
+    )
+}
+
+export default ContactPage
