1c4df372f737c565afd2b412f034646fcf14f1bf
Anastasia Shpit <imshpit@gmail.com>
2020-11-21 18:52:50 +0200

    Added header and footer components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import Footer from  '../components/footer'
+import Header from '../components/header'
+
 const AboutPage = () => {
     return (
         <div>
+            <Header/>
             <h1>About</h1>
             <p>Hi there! You are on my lifestyle & tech blog!</p>
             <p>Need a developer? <Link to="/contact">Contact me</Link></p> 
+            <Footer/>
         </div>
     )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import Footer from '../components/footer'
+import Header from '../components/header'
+
 const ContactPage = () => {
     return (
         <div>
+            <Header/>
             <h1>Contact</h1>
             <p>Reach me via <Link to="https://twitter.com/nastyacodes" target="_blank">Twitter</Link></p>
+            <Footer/>
         </div>
     )
 }
