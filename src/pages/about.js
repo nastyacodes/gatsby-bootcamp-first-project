148e0321844252efcf6f1d45b292b75427775951
Anastasia Shpit <imshpit@gmail.com>
2020-11-23 23:58:58 +0200

    gatsby page layout created and applied

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Footer from  '../components/footer'
-import Header from '../components/header'
+import Layout from '../components/layout'
 
 const AboutPage = () => {
     return (
-        <div>
-            <Header/>
+        <Layout>
             <h1>About</h1>
             <p>Hi there! You are on my lifestyle & tech blog!</p>
             <p>Need a developer? <Link to="/contact">Contact me</Link></p> 
-            <Footer/>
-        </div>
+        </Layout>
     )
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Footer from '../components/footer'
-import Header from '../components/header'
+import Layout from '../components/layout'
 
 const ContactPage = () => {
     return (
-        <div>
-            <Header/>
+        <Layout>
             <h1>Contact</h1>
             <p>Reach me via <Link to="https://twitter.com/nastyacodes" target="_blank">Twitter</Link></p>
-            <Footer/>
-        </div>
+        </Layout>
     )
 }
 
