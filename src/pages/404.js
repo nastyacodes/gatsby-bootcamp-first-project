6f9d86fdc53c1c6304436486935a82755910c347
Anastasia Shpit <imshpit@gmail.com>
2020-11-26 22:54:04 +0200

    Creating a custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const Page404 = () => {
+    return (
+        <Layout>
+            <h1>Unfortunately a page you are looking for isn't existing</h1>
+            <p>Return to the <Link to="/">main page</Link>.</p>
+        </Layout>
+    )
+}
+
+export default Page404
